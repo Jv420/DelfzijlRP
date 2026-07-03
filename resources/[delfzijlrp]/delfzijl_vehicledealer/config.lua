3e5917b81c9366fa1942c4ca60f32c23397502ad
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 11:47:59 +0200

    Voeg vehicledealer configuratie toe

---
+++ resources/[delfzijlrp]/delfzijl_vehicledealer/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Debug = false
+Config.DefaultStored = 1
+
+Config.DutchPlate = {
+    enabled = true,
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    patterns = {
+        '99-XXX-9',
+        'X-999-XX',
+        'XX-999-X',
+        'XXX-99-X'
+    }
+}
