92086fe027c1d6f0508bdcf1bbb996c8b63fcbf3
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 16:38:15 +0200

    Voeg prison manifest toe

---
+++ resources/[delfzijlrp]/delfzijlrp_prison/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Delfzijl RP'
+description 'Delfzijl RP Prison - detentie, taakstraffen, celstatus en rechtbank/MDT-koppeling'
+version '0.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+files {
+    'sql/*.sql'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_target',
+    'oxmysql',
+    'es_extended',
+    'delfzijlrp_identity',
+    'delfzijlrp_mdt_v2',
+    'delfzijlrp_court'
+}
+
+lua54 'yes'
