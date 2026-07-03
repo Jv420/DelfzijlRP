8ffb9d80b0351f076158e6ffa22a899e877bd8cf
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 10:51:07 +0200

    Voeg core configuratie toe

---
+++ resources/[delfzijlrp]/delfzijlrp_core/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.ServerName = 'Delfzijl RP'
+Config.Locale = 'nl'
+Config.Debug = false
+
+Config.StartMoney = {
+    cash = 2500,
+    bank = 15000
+}
+
+Config.DefaultSpawn = vector4(-1037.72, -2737.87, 20.17, 328.0)
