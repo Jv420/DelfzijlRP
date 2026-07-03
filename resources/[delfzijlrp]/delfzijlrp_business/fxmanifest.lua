099dda90763898f297bca6eaa8225cf6089f6e7d
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 12:16:30 +0200

    Voeg business manifest toe

---
+++ resources/[delfzijlrp]/delfzijlrp_business/fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Delfzijl RP'
+description 'Delfzijl RP Business - bedrijven, werknemers, facturen en bedrijfsrekeningen'
+version '0.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+files {
+    'sql/*.sql'
+}
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'es_extended',
+    'delfzijlrp_identity'
+}
+
+lua54 'yes'
