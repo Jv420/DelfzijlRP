446534bc2723a88f69eb73bb221debd9306c98b6
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 12:41:39 +0200

    Voeg jobs manifest toe

---
+++ resources/[delfzijlrp]/delfzijlrp_jobs/fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Delfzijl RP'
+description 'Delfzijl RP Jobs - legale burgerbanen en werkopdrachten'
+version '0.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+files {
+    'sql/*.sql'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_target',
+    'ox_inventory',
+    'oxmysql',
+    'es_extended'
+}
+
+lua54 'yes'
