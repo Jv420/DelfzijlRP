3a8f3d87d3335901d6547e89444de516a1bad173
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 11:33:42 +0200

    Voeg delfzijlrp_contraband manifest toe

---
+++ resources/[delfzijlrp]/delfzijlrp_contraband/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Delfzijl RP'
+description 'Delfzijl RP Contraband - fictieve RP smokkelwaar en verkoopzones'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_target',
+    'ox_inventory',
+    'es_extended'
+}
+
+lua54 'yes'
