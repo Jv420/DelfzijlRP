735d621f6aa021497b6faa4deba0973ee519512d
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 11:53:08 +0200

    Voeg platepack placeholder manifest toe

---
+++ resources/[delfzijlrp]/delfzijlrp_platepack/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Delfzijl RP'
+description 'Delfzijl RP Plate Pack - placeholder voor Nederlandse gele kenteken textures'
+version '1.0.0'
+
+files {
+    'stream/*.ytd'
+}
+
+lua54 'yes'
