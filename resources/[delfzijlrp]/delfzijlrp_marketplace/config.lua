cd145e6ab95a51abee3ad6c6c3c471e7d584c377
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 14:09:32 +0200

    Voeg marketplace configuratie toe

---
+++ resources/[delfzijlrp]/delfzijlrp_marketplace/config.lua
@@ -0,0 +1,36 @@
+Config = {}
+
+Config.Debug = false
+Config.Command = 'marktplaats'
+Config.PhoneCommand = 'mp'
+Config.ListingFee = 100
+Config.MaxTitleLength = 64
+Config.MaxDescriptionLength = 500
+
+Config.Categories = {
+    vehicle = 'Voertuigen',
+    house = 'Woningen',
+    item = 'Items',
+    service = 'Diensten',
+    business = 'Bedrijven',
+    other = 'Overig'
+}
+
+Config.Status = {
+    active = 'Actief',
+    reserved = 'Gereserveerd',
+    sold = 'Verkocht',
+    cancelled = 'Geannuleerd'
+}
+
+Config.Text = {
+    noMoney = 'Je hebt niet genoeg geld.',
+    created = 'Advertentie geplaatst.',
+    updated = 'Advertentie bijgewerkt.',
+    cancelled = 'Advertentie geannuleerd.',
+    interestSent = 'Interesse verstuurd naar verkoper.',
+    invalidInput = 'Ongeldige invoer.',
+    notOwner = 'Deze advertentie is niet van jou.',
+    notFound = 'Advertentie niet gevonden.',
+    noListings = 'Geen advertenties gevonden.'
+}
