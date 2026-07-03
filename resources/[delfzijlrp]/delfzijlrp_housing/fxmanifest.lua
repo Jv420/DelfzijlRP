3eb59480b639fe68ead1c2b51e101a6afa59e306
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-03 12:37:20 +0200

    Voeg housing manifest toe

---
+++ resources/[delfzijlrp]/delfzijlrp_housing/fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Delfzijl RP'
+description 'Delfzijl RP Housing - woningen, huur/koop, sleutels en opslag'
+version '0.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+files {
+    'sql/*.sql'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_target',
+    'ox_inventory',
+    'oxmysql',
+    'es_extended',
+    'delfzijlrp_identity'
+}
+
+lua54 'yes'
