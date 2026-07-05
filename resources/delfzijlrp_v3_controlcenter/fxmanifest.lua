927fc0645da9314a6d81884bdd341c534f286ac8
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 23:48:18 +0200

    Laad DRCC 100 procent tools

--- resources/delfzijlrp_v3_controlcenter/fxmanifest.lua
+++ resources/delfzijlrp_v3_controlcenter/fxmanifest.lua
@@ -10,13 +10,15 @@ shared_scripts {
 
 client_scripts {
     'client.lua',
-    'client_tools.lua'
+    'client_tools.lua',
+    'client_events.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server.lua',
-    'server_tools.lua'
+    'server_tools.lua',
+    'server_events.lua'
 }
 
 dependencies {
