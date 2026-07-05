1cd520b8634c42069e2c52fc4ea4b96c4a440ef1
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 23:33:51 +0200

    Laad DRCC Premium tools

--- resources/delfzijlrp_v3_controlcenter/fxmanifest.lua
+++ resources/delfzijlrp_v3_controlcenter/fxmanifest.lua
@@ -9,12 +9,14 @@ shared_scripts {
 }
 
 client_scripts {
-    'client.lua'
+    'client.lua',
+    'client_tools.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server.lua'
+    'server.lua',
+    'server_tools.lua'
 }
 
 dependencies {
