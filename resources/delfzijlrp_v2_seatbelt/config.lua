d23a3934bc36e6e416e095d170597d41f61eb817
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 21:58:45 +0200

    Voeg seatbelt config toe

---
+++ resources/delfzijlrp_v2_seatbelt/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config.Command = 'gordel'
+Config.DefaultKey = 'B'
+Config.Notify = true
+Config.DisableExitWhenBuckled = true
+Config.EjectCheck = true
+Config.MinEjectSpeedKmh = 65.0
+Config.SpeedDropForEject = 45.0
+
+Config.Text = {
+    on = 'Gordel om.',
+    off = 'Gordel af.',
+    hint = 'Druk B om je gordel om/af te doen.'
+}
