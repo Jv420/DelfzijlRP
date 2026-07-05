4750f2ecb46952a81933153f8dbc527aeefd3208
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 22:01:19 +0200

    Voeg vehiclelock config toe

---
+++ resources/delfzijlrp_v2_vehiclelock/config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+Config.LockCommand = 'lockcar'
+Config.TrunkCommand = 'kofferbak'
+Config.LockKey = 'U'
+Config.TrunkKey = 'K'
+Config.Range = 8.0
+Config.FlashLights = true
+Config.HornSound = true
+
+Config.Text = {
+    noVehicle = 'Geen voertuig dichtbij gevonden.',
+    noKey = 'Je hebt geen sleutel van dit voertuig.',
+    locked = 'Voertuig op slot.',
+    unlocked = 'Voertuig open.',
+    trunkOpen = 'Kofferbak open.',
+    trunkClosed = 'Kofferbak dicht.'
+}
