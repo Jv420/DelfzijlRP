aff791ad198d492123ded6acade6450aaf003a5a
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 22:23:23 +0200

    Voeg papieren config toe

---
+++ resources/delfzijlrp_v2_papieren/config.lua
@@ -0,0 +1,35 @@
+Config = {}
+
+Config.Commands = {
+    giveId = 'geefidkaart',
+    giveLicense = 'geefrijbewijs',
+    giveBusTicket = 'buskaartje',
+    giveVehicleDocs = 'voertuigpapieren'
+}
+
+Config.Prices = {
+    idkaart = 150,
+    rijbewijs = 350,
+    buskaartje = 25,
+    kentekenbewijs = 50,
+    verzekeringsbewijs = 75,
+    autosleutel = 0
+}
+
+Config.Items = {
+    idkaart = 'idkaart',
+    rijbewijs = 'rijbewijs',
+    buskaartje = 'buskaartje',
+    kentekenbewijs = 'kentekenbewijs',
+    verzekeringsbewijs = 'verzekeringsbewijs',
+    autosleutel = 'autosleutel'
+}
+
+Config.Text = {
+    noMoney = 'Je hebt niet genoeg geld.',
+    given = 'Papier/item ontvangen.',
+    noVehicle = 'Geen voertuig gevonden.',
+    noOwner = 'Dit voertuig staat niet op jouw naam.',
+    missingItem = 'Item bestaat nog niet in ox_inventory.',
+    invalid = 'Ongeldige invoer.'
+}
