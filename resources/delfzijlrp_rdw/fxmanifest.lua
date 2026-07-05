46f9792997e0d29d0a11983139ad5af118a3cfd2
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 14:39:19 +0200

    Werk RDW manifest uit

--- resources/delfzijlrp_rdw/fxmanifest.lua
+++ resources/delfzijlrp_rdw/fxmanifest.lua
@@ -1,4 +1,33 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'Delfzijl RP'
+description 'Delfzijl RP RDW'
+version '0.1.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+files {
+    'sql/rdw.sql'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_target',
+    'oxmysql',
+    'es_extended'
+}
+
 lua54 'yes'
