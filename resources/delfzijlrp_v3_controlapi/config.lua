98de45baac7ebc83fdf4ab0d12e5de3796a59356
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-06 00:08:37 +0200

    Voeg DRCC Control API config toe

---
+++ resources/delfzijlrp_v3_controlapi/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+-- VERANDER DEZE KEY DIRECT NA INSTALLATIE
+Config.ApiKey = 'CHANGE_ME_SUPER_SECRET_KEY'
+
+Config.AllowedOrigins = {
+    'https://delfzijl-rp.vercel.app',
+    'https://delfzijlrp.nl',
+    'http://localhost:3000'
+}
+
+Config.AllowAllOrigins = true
