acda4acc83bd841f9fc98ccd9d29d401cd8703dc
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 21:24:35 +0200

    Voeg platepack v2 config toe

---
+++ resources/[delfzijlrp]/delfzijlrp_platepack/config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.Debug = true
+Config.TestCommand = 'platepacktest'
+Config.ForcePlateIndexOnTest = true
+Config.TestPlateText = 'NL-123-X'
+
+Config.Messages = {
+    started = '^2[Delfzijl RP PlatePack v2]^7 Resource gestart. Controleer of stream/vehshare.ytd aanwezig is.',
+    test = 'PlatePack test uitgevoerd. Als de plaat niet geel is, wordt vehshare.ytd niet goed gestreamd of overschreven door een andere resource.'
+}
