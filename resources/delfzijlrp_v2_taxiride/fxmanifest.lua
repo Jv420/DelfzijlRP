36ba0e30064f488a9a3106526a84fe6d3db90e48
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 21:10:58 +0200

    Voeg taxiride manifest toe

---
+++ resources/delfzijlrp_v2_taxiride/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_target',
+    'es_extended'
+}
