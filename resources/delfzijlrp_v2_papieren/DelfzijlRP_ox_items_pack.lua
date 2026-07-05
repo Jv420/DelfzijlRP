39e0e6ac9292c973e797c9e03e9b16a00f040df3
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 22:29:21 +0200

    Voeg Delfzijl RP ox items pack toe

---
+++ resources/delfzijlrp_v2_papieren/DelfzijlRP_ox_items_pack.lua
@@ -0,0 +1,71 @@
+-- Delfzijl RP ox_inventory items pack
+-- Kopieer deze regels in: ox_inventory/data/items.lua
+
+['idkaart'] = {
+    label = 'Delfzijl ID-kaart',
+    weight = 20,
+    stack = false,
+    close = true,
+},
+
+['rijbewijs'] = {
+    label = 'Rijbewijs B',
+    weight = 20,
+    stack = false,
+    close = true,
+},
+
+['buskaartje'] = {
+    label = 'Buskaartje',
+    weight = 5,
+    stack = true,
+    close = true,
+},
+
+['kentekenbewijs'] = {
+    label = 'Kentekenbewijs',
+    weight = 25,
+    stack = false,
+    close = true,
+},
+
+['verzekeringsbewijs'] = {
+    label = 'Verzekeringsbewijs',
+    weight = 25,
+    stack = false,
+    close = true,
+},
+
+['autosleutel'] = {
+    label = 'Autosleutel',
+    weight = 30,
+    stack = false,
+    close = true,
+},
+
+['coffee'] = { label = 'Koffie', weight = 100, stack = true, close = true },
+['cola'] = { label = 'Cola', weight = 100, stack = true, close = true },
+['tosti'] = { label = 'Tosti', weight = 150, stack = true, close = true },
+['borrelhapjes'] = { label = 'Borrelhapjes', weight = 250, stack = true, close = true },
+['appelgebak'] = { label = 'Appelgebak', weight = 200, stack = true, close = true },
+['uitsmijter'] = { label = 'Uitsmijter', weight = 250, stack = true, close = true },
+
+['alanya_broodje'] = { label = 'Turks broodje', weight = 250, stack = true, close = true },
+['alanya_kapsalon'] = { label = 'Kapsalon', weight = 350, stack = true, close = true },
+['alanya_pizza'] = { label = 'Pizza', weight = 400, stack = true, close = true },
+
+['sharazan_burger'] = { label = 'Burger', weight = 250, stack = true, close = true },
+['sharazan_patat'] = { label = 'Patat', weight = 250, stack = true, close = true },
+['sharazan_tosti_leroy'] = { label = 'Tosti Leroy', weight = 220, stack = true, close = true },
+
+['milas_roti'] = { label = 'Roti rol', weight = 300, stack = true, close = true },
+['milas_bara'] = { label = 'Bara', weight = 200, stack = true, close = true },
+['milas_saoto'] = { label = 'Saoto soep', weight = 300, stack = true, close = true },
+
+['brownie'] = { label = 'Brownie', weight = 150, stack = true, close = true },
+['smoothie'] = { label = 'Smoothie', weight = 150, stack = true, close = true },
+['ng_pack'] = { label = 'NewGen pakket', weight = 250, stack = true, close = true },
+
+['repairkit'] = { label = 'Reparatiekit', weight = 500, stack = true, close = true },
+['cleaningkit'] = { label = 'Schoonmaakset', weight = 250, stack = true, close = true },
+['jerrycan'] = { label = 'Jerrycan', weight = 1000, stack = false, close = true },
