70b995dd320ad1c2b890e227ee7654eb3119941b
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 21:24:19 +0200

    Upgrade platepack naar v2 met testcommand

--- resources/[delfzijlrp]/delfzijlrp_platepack/fxmanifest.lua
+++ resources/[delfzijlrp]/delfzijlrp_platepack/fxmanifest.lua
@@ -1,12 +1,20 @@
 fx_version 'cerulean'
 game 'gta5'
 
+lua54 'yes'
+
 author 'Delfzijl RP'
-description 'Delfzijl RP Plate Pack - Nederlandse gele kentekenplaten'
-version '1.0.1'
+description 'Delfzijl RP PlatePack v2 - Nederlandse gele kentekenplaten'
+version '2.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
 
 files {
     'stream/vehshare.ytd'
 }
-
-lua54 'yes'
