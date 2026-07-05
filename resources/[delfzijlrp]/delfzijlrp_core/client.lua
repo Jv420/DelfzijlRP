51aedfedb9faa24edc106ece8e793bd7402b36fa
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-05 09:31:13 +0200

    Voeg loading screen en spawn fallback toe aan core client

--- resources/[delfzijlrp]/delfzijlrp_core/client.lua
+++ resources/[delfzijlrp]/delfzijlrp_core/client.lua
@@ -1,7 +1,43 @@
+local hasSpawned = false
+
+local function closeLoadingScreen()
+    ShutdownLoadingScreen()
+    ShutdownLoadingScreenNui()
+    DoScreenFadeIn(800)
+end
+
+local function safeSpawnFallback()
+    if hasSpawned then return end
+    if not exports.spawnmanager then return end
+
+    exports.spawnmanager:setAutoSpawn(false)
+    exports.spawnmanager:spawnPlayer({
+        x = Config.Spawn.x,
+        y = Config.Spawn.y,
+        z = Config.Spawn.z,
+        heading = Config.Spawn.w or 0.0,
+        model = `mp_m_freemode_01`,
+        skipFade = false
+    }, function()
+        hasSpawned = true
+        closeLoadingScreen()
+        print(('[%s] Spawn fallback uitgevoerd.'):format(Config.ServerName))
+    end)
+end
+
+AddEventHandler('playerSpawned', function()
+    hasSpawned = true
+    closeLoadingScreen()
+end)
+
 CreateThread(function()
     while not NetworkIsSessionStarted() do
         Wait(250)
     end
 
     print(('[%s] Core client geladen.'):format(Config.ServerName))
+    closeLoadingScreen()
+
+    Wait(8000)
+    safeSpawnFallback()
 end)
