ff6a4beaaca68241286c82602d9d192e45ff5373
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-06 17:18:42 +0200

    Voeg Identity Engine config toe

---
+++ resources/delfzijlrp_v3_identity_engine/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Command = 'identiteit'
+
+Config.PhonePrefix = '06'
+Config.DefaultAddress = 'Geen adres geregistreerd'
+
+Config.Text = {
+    created = 'Identiteit aangemaakt.',
+    updated = 'Identiteit bijgewerkt.',
+    noData = 'Geen identiteit gevonden.',
+    invalid = 'Ongeldige invoer.'
+}
