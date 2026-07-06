1967de1238bfd2e829e49191b9eb947111b3f4bc
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-06 17:26:40 +0200

    Voeg RDW Premium config toe

---
+++ resources/delfzijlrp_v3_rdw_premium/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Command = 'rdwplus'
+Config.DefaultApkDays = 30
+Config.DefaultInsurance = 'WA'
+
+Config.Text = {
+    notFound = 'Geen voertuig gevonden.',
+    registered = 'Voertuig geregistreerd.',
+    updated = 'RDW status bijgewerkt.',
+    invalid = 'Ongeldige invoer.'
+}
