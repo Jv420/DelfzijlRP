adbabe6ff889165b65d10af8d775fd8adff07c1c
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-06 17:33:16 +0200

    Voeg voertuig eco config toe

---
+++ resources/delfzijlrp_v3_veh_eco/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Command = 'voertuigeco'
+Config.DefaultValue = 15000
+Config.DefaultCondition = 100
+Config.DefaultService = 100
+
+Config.Text = {
+    notFound = 'Geen voertuigprofiel gevonden.',
+    created = 'Voertuigprofiel aangemaakt.',
+    updated = 'Voertuigprofiel bijgewerkt.',
+    invalid = 'Ongeldige invoer.'
+}
