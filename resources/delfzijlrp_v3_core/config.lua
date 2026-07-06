a32cd0baf2c2829ee7abc12d9d183c8a35bfdae0
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-06 17:11:42 +0200

    Voeg DRP Core config toe

---
+++ resources/delfzijlrp_v3_core/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.Command = 'drpprofiel'
+Config.Debug = false
+
+Config.Profile = {
+    citizenPrefix = 'DRP',
+    startReputation = 0,
+    startTrust = 50
+}
+
+Config.Text = {
+    profileReady = 'Profiel geladen.',
+    profileCreated = 'Nieuw burgerprofiel aangemaakt.',
+    noProfile = 'Geen profiel gevonden.',
+    done = 'Actie uitgevoerd.'
+}
