52408e2e0e95a3c8d7a9738d9da1198ecb791396
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-06 18:08:13 +0200

    Voeg luxe dealer manifest toe

---
+++ resources/delfzijlrp_v3_lux_dealer/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'ox_target',
+    'oxmysql',
+    'es_extended',
+    'delfzijlrp_v3_business_core',
+    'delfzijlrp_v3_rdw_premium',
+    'delfzijlrp_v3_veh_eco'
+}
