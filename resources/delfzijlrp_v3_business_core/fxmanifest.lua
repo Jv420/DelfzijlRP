7d29cbf7084f5b8185ec9e9668fbd59e5dee6bba
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-06 17:46:04 +0200

    Voeg Business Core manifest toe

---
+++ resources/delfzijlrp_v3_business_core/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'es_extended',
+    'delfzijlrp_v3_core',
+    'delfzijlrp_v3_identity_engine'
+}
