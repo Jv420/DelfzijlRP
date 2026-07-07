bcc2fcd31f05522845f415436cd83c1d68a06760
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-07 11:40:40 +0200

    Voeg Business Manager config toe

---
+++ resources/delfzijlrp_v3_business_manager/config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+Config.Command = 'bizmanager'
+Config.Debug = false
+
+Config.AllowedRoles = {
+    owner = true,
+    manager = true
+}
+
+Config.Text = {
+    noAccess = 'Je hebt geen management toegang.',
+    noBusiness = 'Bedrijf niet gevonden.',
+    invalid = 'Ongeldige invoer.',
+    updated = 'Bijgewerkt.',
+    title = 'Business Manager'
+}
