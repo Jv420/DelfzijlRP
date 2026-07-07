773876009d9e5872b411b57de07893ffaf493ce2
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-07 11:34:05 +0200

    Voeg BizDash config toe

---
+++ resources/delfzijlrp_v3_bizdash/config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+Config.Command = 'bizdash'
+Config.Debug = false
+
+Config.RestaurantIds = {
+    alanya = true,
+    sharazan = true,
+    milas = true,
+    cafe_centrum = true,
+    stad_lande = true
+}
+
+Config.Text = {
+    noAccess = 'Je hebt geen toegang tot dit dashboard.',
+    noBusiness = 'Bedrijf niet gevonden.',
+    invalid = 'Ongeldige invoer.'
+}
