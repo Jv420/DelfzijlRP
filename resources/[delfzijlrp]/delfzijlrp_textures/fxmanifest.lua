ae72f34ed7dad43e1d72e718b23b450e62b15fc2
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-09 15:51:06 +0200

    Add Delfzijl RP texture pack resource manifest

---
+++ resources/[delfzijlrp]/delfzijlrp_textures/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Delfzijl RP / Jochem Visser'
+description 'Delfzijl RP Texture Pack - Nederlandse RP uitstraling voor FiveM'
+version '1.0.0'
+
+-- Deze resource streamt alleen textures/models vanuit de stream-map.
+-- Plaats hier je eigen of permissief gelicentieerde .ytd/.ydr/.yft bestanden.
+
+files {
+    'README.md',
+    'config/texturepack.cfg'
+}
+
+-- Geen client/server scripts nodig voor pure texture streaming.
