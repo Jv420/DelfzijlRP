ab8801efd4549912a7c23d40851b6c6ea433900f
VloeiNetwork <136645216+Jv420@users.noreply.github.com>
2026-07-09 18:15:27 +0200

    Add Jumbo Premier FiveM resource manifest

---
+++ resources/[vehicles]/delfzijlrp_jumbo_premier/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Delfzijl RP'
+description 'Jumbo bezorgvoertuig texture replace voor Premier'
+version '1.0.0'
+
+files {
+    'stream/premier.ytd',
+    'stream/premier+hi.ytd'
+}
